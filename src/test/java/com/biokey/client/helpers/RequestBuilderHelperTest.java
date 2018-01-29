d6d05bc93e7adf563841058c5fc77d977d39b562
Tony Wu <tony_yz_wu@hotmail.com>
2018-01-28 19:26:04 -0500

    Add the unit tests for request builder helper.

---
+++ src/test/java/com/biokey/client/helpers/RequestBuilderHelperTest.java
@@ -0,0 +1,65 @@
+package com.biokey.client.helpers;
+
+import com.biokey.client.constants.AppConstants;
+import com.biokey.client.constants.StatusConstants;
+import com.biokey.client.models.ClientStateModel;
+import com.biokey.client.models.pojo.ClientStatusPojo;
+import com.biokey.client.models.pojo.KeyStrokePojo;
+import com.biokey.client.models.pojo.KeyStrokesPojo;
+import com.biokey.client.models.pojo.TypingProfilePojo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(PowerMockRunner.class)
+public class RequestBuilderHelperTest {
+
+    @InjectMocks
+    private RequestBuilderHelper underTest;
+
+    @Mock
+    private ClientStateModel state;
+
+    private static final String ACCESS_TOKEN = "ACCESS_TOKEN";
+    private static final String TYPING_PROFILE_ID = "TYPING_PROFILE_ID";
+    private static final ClientStatusPojo CLIENT_STATUS_POJO =
+            new ClientStatusPojo(
+                    new TypingProfilePojo(TYPING_PROFILE_ID, "","","",new float[] {}, (String challenge) -> false),
+                    StatusConstants.UNLOCKED,
+                    ACCESS_TOKEN,
+                    0);
+
+    private static final KeyStrokePojo KEY_STROKE_POJO = new KeyStrokePojo('t', true, 1);
+    private static final KeyStrokePojo OTHER_KEY_STROKE_POJO = new KeyStrokePojo('b', false, 2);
+    private static final KeyStrokesPojo KEY_STROKES_POJO = new KeyStrokesPojo();
+
+    private static final String EXPECTED_KEY_STROKE_JSON = "[{\"character\":\"t\",\"keyDown\":true,\"timestamp\":1,\"typingProfile\":\"TYPING_PROFILE_ID\"},{\"character\":\"b\",\"keyDown\":false,\"timestamp\":2,\"typingProfile\":\"TYPING_PROFILE_ID\"}]";
+
+    @BeforeClass
+    public static void loadData() {
+        KEY_STROKES_POJO.getKeyStrokes().add(KEY_STROKE_POJO);
+        KEY_STROKES_POJO.getKeyStrokes().add(OTHER_KEY_STROKE_POJO);
+    }
+
+    @Test
+    public void GIVEN_accessToken_WHEN_headerMapWithToken_THEN_expectedResult() {
+        Mockito.when(state.getCurrentStatus()).thenReturn(CLIENT_STATUS_POJO);
+        assertTrue("header map has expected value for access token",
+                underTest.headerMapWithToken().getFirst(AppConstants.SERVER_TOKEN_HEADER).equals(ACCESS_TOKEN));
+    }
+
+    @Test
+    public void GIVEN_keyStrokesAndTypingProfile_WHEN_headerMapWithToken_THEN_expectedResult() throws JsonProcessingException {
+        Mockito.when(state.getCurrentStatus()).thenReturn(CLIENT_STATUS_POJO);
+        Mockito.when(state.getOldestKeyStrokes()).thenReturn(KEY_STROKES_POJO);
+        assertTrue("header map has expected value for access token",
+                underTest.requestBodyToPostKeystrokes().get("keystrokes").equals(EXPECTED_KEY_STROKE_JSON));
+    }
+}
