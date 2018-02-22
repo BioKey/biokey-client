7c0c753fb2eccb780b38f007a3686b616dd8b3a2
mkucera3 <mkucera3@uwo.ca>
2018-02-22 15:58:01 -0500

    Testing gitignore

--- src/main/java/com/biokey/client/constants/Credentials.java
+++ src/main/java/com/biokey/client/constants/Credentials.java
@@ -4,9 +4,9 @@ package com.biokey.client.constants;
  * Credentials for web services.
  */
 public class Credentials {
-    public static final String AWS_ACCESS_KEY_ID = "AKIAIBZJPRVMSRYPRBKA";
-    public static final String AWS_SECRET_ACCESS_KEY = "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";
-    public static final String TWILIO_ACCOUNT_SID = "AC6135115ec55c24ab759c1ae658b2fdfb";
-    public static final String TWILIO_AUTH_TOKEN = "a531787d174d594e7be4e6c151cee85d";
-    public static final String TWILIO_FROM_PHONE_NUMBER = "+16473602026";
+    public static final String AWS_ACCESS_KEY_ID = "";
+    public static final String AWS_SECRET_ACCESS_KEY = "";
+    public static final String TWILIO_ACCOUNT_SID = "";
+    public static final String TWILIO_AUTH_TOKEN = "";
+    public static final String TWILIO_FROM_PHONE_NUMBER = "";
 }
